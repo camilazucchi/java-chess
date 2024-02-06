9a11d02a88bf9ab1238a9d8ab62643747613fcc8
camilazci <camilazucchi@outlook.com>
2024-02-06 17:29:08 -0300

    feat: implement program class to test position toString method

---
+++ src/application/Program.java
@@ -0,0 +1,12 @@
+package application;
+
+import boardgame.Position;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Position position = new Position(5, 3);
+        System.out.println(position);
+
+    }
+}
