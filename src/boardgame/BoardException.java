f3af1e21e0f03464eccd4d0b6f9e8cc2d33e72d1
camilazci <camilazucchi@outlook.com>
2024-02-16 10:05:56 -0300

    feat: implement personalized exception BoardException

---
+++ src/boardgame/BoardException.java
@@ -0,0 +1,10 @@
+package boardgame;
+
+// RuntimeException é uma exceção opcional de ser tratada.
+public class BoardException extends RuntimeException {
+
+    public BoardException(String message) {
+        super(message);
+    }
+
+}
